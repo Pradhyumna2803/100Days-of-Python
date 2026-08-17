9f4ac5106efde5ed632f6d5af1c239086a929c55
Pradhyumna <pradhyumnapradhyu10@gmail.com>
2026-08-17 19:54:04 +0530

    learning inheritence with small examples

--- Day 19/File2.py
+++ Day 19/File2.py
@@ -21,3 +21,22 @@ class Dog(Animal):
 
     def sound(self):
         print(f"{self.name} barks")
+
+    def greet(self):
+        print(f'{self.name} starts wagging tail and smiles!')
+
+
+class Cat(Animal):
+    def __init__(self, name,age,breed):
+        super().__init__(name)
+        self.age = age
+        self.breed = breed
+
+    def details(self):
+        print(f"This is {self.name} , a {self.breed}")
+
+    def sound(self):
+        print(f'{self.name} mews')
+
+    def greet(self):
+        print(f'You greet {self.name} !! And you are ignored!! ')
--- Day 19/main.py
+++ Day 19/main.py
@@ -1,6 +1,12 @@
 from File2 import Animal
 from File2 import Dog
+from File2 import Cat
 
 oreo = Dog('Oreo',3,'Golden Retriever')
 oreo.info()
 oreo.sound()
+oreo.greet()
+
+tom = Cat('Tom',5,'American Cat')
+tom.details()
+tom.greet()
